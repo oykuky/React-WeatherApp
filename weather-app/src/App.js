90b87f4e2bc04f2ba6ffec7eddad4618d88b4075
Öykü Kaya <oykuky001@gmail.com>
2023-09-07 19:55:42 +0300

    updated

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import axios from 'axios'
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+
+  },[])
   return (
     <div className="App">
-      
+      <h2>hello</h2>
     </div>
   );
 }
